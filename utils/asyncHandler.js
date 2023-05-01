c495e77979696f75823b764dec8f92e7db74c2e0
jay bhogayata <jaybhogayata53@gmail.com>
2023-05-01 18:07:53 +0530

    add async handler

---
+++ utils/asyncHandler.js
@@ -0,0 +1,10 @@
+module.exports = (fn) => async (req, res, next) => {
+  try {
+    await fn(req, res, next);
+  } catch (error) {
+    res.status(error.code || 500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
