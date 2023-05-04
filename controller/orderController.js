ca450464340c87b48d8d9a2ae53293af568a2c76
jay bhogayata <jaybhogayata53@gmail.com>
2023-05-04 21:15:35 +0530

    add async Handler in order controllers

--- controller/orderController.js
+++ controller/orderController.js
@@ -1,118 +1,92 @@
-const Order = require('../models/order');
-const Product = require('../models/product');
-const CustomError = require('../utils/CustomError');
+const Order = require("../models/order");
+const Product = require("../models/product");
+const CustomError = require("../utils/CustomError");
+const asyncHandler = require("../utils/asyncHandler");
 
-exports.createOrder = async (req, res, next) => {
-    try {
-        const {
-            shippingInfo,
-            orderItems,
-            paymentInfo,
-            texAmount,
-            shippingAmount,
-            totalAmount
-        } = req.body
+exports.createOrder = asyncHandler(async (req, res) => {
+  const {
+    shippingInfo,
+    orderItems,
+    paymentInfo,
+    texAmount,
+    shippingAmount,
+    totalAmount,
+  } = req.body;
 
-        const newOrder = await Order.create({
-            shippingInfo,
-            orderItems,
-            paymentInfo,
-            texAmount,
-            shippingAmount,
-            totalAmount,
-            user: req.user._id
-        })
-        res.status(200).json({ success: true, newOrder })
-    } catch (error) {
-        console.log(error);
-    }
+  const newOrder = await Order.create({
+    shippingInfo,
+    orderItems,
+    paymentInfo,
+    texAmount,
+    shippingAmount,
+    totalAmount,
+    user: req.user._id,
+  });
+  res.status(200).json({ success: true, newOrder });
+});
 
-}
+exports.getOneOrder = asyncHandler(async (req, res) => {
+  // mongo has populate method which is use to drill down more info
+  const order = await Order.findById(req.params.id);
 
-exports.getOneOrder = async (req, res, next) => {
-    try {
-        // mongo has populate method which is use to drill down more info
-        const order = await Order.findById(req.params.id)
+  if (!order) {
+    return next(new CustomError("Order not found with this id!", 404));
+  }
 
-        if (!order) {
-            return next(new CustomError("Order not found with this id!", 404))
-        }
+  res.status(200).json({
+    success: true,
+    order,
+  });
+});
 
-        res.status(200).json({
-            success: true,
-            order
-        })
-    } catch (error) {
-        console.log(error);
-    }
-}
+exports.getMyOrder = asyncHandler(async (req, res) => {
+  const order = await Order.find({ user: req.user._id });
+  if (!order) {
+    return next(new CustomError("Order not found with this id!", 404));
+  }
 
-exports.getMyOrder = async (req, res, next) => {
-    try {
-        const order = await Order.find({ user: req.user._id })
-        if (!order) {
-            return next(new CustomError("Order not found with this id!", 404))
-        }
+  res.status(200).json({
+    success: true,
+    order,
+  });
+});
 
-        res.status(200).json({
-            success: true,
-            order
-        })
-    } catch (error) {
-        console.log(error);
-    }
-}
+exports.adminAllOrders = asyncHandler(async (req, res) => {
+  const order = await Order.find();
 
-exports.adminAllOrders = async (req, res, next) => {
-    try {
-        const order = await Order.find()
+  res.status(200).json({
+    success: true,
+    order,
+  });
+});
 
-        res.status(200).json({
-            success: true,
-            order
-        })
-    } catch (error) {
-        console.log(error);
-    }
-}
-
-exports.adminUpdateorders = async (req, res, next) => {
-    try {
-        const order = await Order.findById(req.params.id)
-        const { orderStatus } = req.body
-        if (order.status === "Delivered") {
-            return next(new CustomError("Order is already delivered!", 404))
-        }
-        order.status = orderStatus
-        // if order delivered then update invantory 
-        order.orderItems.forEach(async prod => {
-            updateProduct(prod.product, prod.quantity)
-        })
-        order.save()
-
-    } catch (error) {
-        console.log(error);
-    }
-}
+exports.adminUpdateorders = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id);
+  const { orderStatus } = req.body;
+  if (order.status === "Delivered") {
+    return next(new CustomError("Order is already delivered!", 404));
+  }
+  order.status = orderStatus;
+  // if order delivered then update invantory
+  order.orderItems.forEach(async (prod) => {
+    updateProduct(prod.product, prod.quantity);
+  });
+  order.save();
+});
 
 async function updateProduct(productId, quantity) {
-    const product = await Product.findById(productId)
-    product.stock -= quantity
-    await product.save({
-        validateBeforeSave: false
-    })
+  const product = await Product.findById(productId);
+  product.stock -= quantity;
+  await product.save({
+    validateBeforeSave: false,
+  });
 }
 
-exports.adminDeleteorders = async (req, res, next) => {
-    try {
-        const order = await Order.findById(req.params.id)
-        order.remove()
+exports.adminDeleteorders = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id);
+  order.remove();
 
-        res.status(200).json({
-            success: true
-        })
-
-    } catch (error) {
-        console.log(error);
-    }
-}
+  res.status(200).json({
+    success: true,
+  });
+});
