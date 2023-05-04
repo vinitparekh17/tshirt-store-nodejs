84c9c78b7ae307ce20c84089846dc40211a0387f
jay bhogayata <jaybhogayata53@gmail.com>
2023-05-04 21:20:20 +0530

    add async Handler in product and payment controller

--- controller/paymentController.js
+++ controller/paymentController.js
@@ -1,61 +1,49 @@
-const stripe = require('stripe')(process.env.STRIPE_KEY);
-const Razorpay = require('razorpay');
+const stripe = require("stripe")(process.env.STRIPE_KEY);
+const Razorpay = require("razorpay");
+const asyncHandler = require("../utils/asyncHandler");
 
-exports.sendStripeKey = async (req, res, next) => {
-    try {
-        res.status(200).json({
-            stripeKey: process.env.STRIPE_KEY
-        })
-    } catch (error) {
-        console.log(error);
-    }
-}
+exports.sendStripeKey = asyncHandler(async (req, res) => {
+  res.status(200).json({
+    stripeKey: process.env.STRIPE_KEY,
+  });
+});
 
-exports.getSripePayment = async (req, res, next) => {
-    try {
+exports.getSripePayment = asyncHandler(async (req, res) => {
+  const paymentIntent = await stripe.paymentIntent.create({
+    amount: req.body.amount,
+    currency: "INR",
+    metadata: { integration_check: "accept_a_payment" },
+  });
 
-        const paymentIntent = await stripe.paymentIntent.create({
-            amount: req.body.amount,
-            currency: "INR",
-            metadata: {integration_check: 'accept_a_payment'}
-        })
+  res.status(200).json({
+    success: true,
+    client_secret: paymentIntent.client_secret,
+    //can send id as well
+  });
+});
 
-        res.status(200).json({
-            success: true,
-            client_secret: paymentIntent.client_secret
-            //can send id as well   
-        })
-    } catch (error) {
-        console.log(error);
-    }
-}
-
-exports.sendRazorpayKey = async (req, res, next) => {
-    try {
-        res.status(200).json({
-            stripeKey: process.env.RAZORPAY_KEY
-        })
-    } catch (error) {
-        console.log(error);
-    }
-}
+exports.sendRazorpayKey = asyncHandler(async (req, res) => {
+  res.status(200).json({
+    stripeKey: process.env.RAZORPAY_KEY,
+  });
+});
 
 exports.getRayzorpayPayment = async (req, res, next) => {
-    const instance = new Razorpay({
-        key_id: process.env.RAZORPAY_KEY,
-        key_secret: process.env.RAZORPAY_SECRET
-    })
+  const instance = new Razorpay({
+    key_id: process.env.RAZORPAY_KEY,
+    key_secret: process.env.RAZORPAY_SECRET,
+  });
 
-    let amount = req.body.amount;
-    let option = {
-        amount: amount * 100,
-        currency: "INR",
-    }
-    const myOrder = await instance.orders.create(option)
-    
-    res.status(200).json({
-        success: true,
-        amount: req.body.amount,
-        order: myOrder
-    })
-}
+  let amount = req.body.amount;
+  let option = {
+    amount: amount * 100,
+    currency: "INR",
+  };
+  const myOrder = await instance.orders.create(option);
+
+  res.status(200).json({
+    success: true,
+    amount: req.body.amount,
+    order: myOrder,
+  });
+};
