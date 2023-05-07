1a39168f1c79271767194a534162724ce4874ad5
jay bhogayata <jaybhogayata53@gmail.com>
2023-05-07 22:02:45 +0530

    add strictQuery option to mongoose to remove error

--- config/db.js
+++ config/db.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 require('dotenv').config()
 exports.connect = () => {
+    mongoose.set('strictQuery',true)
     mongoose.connect(process.env.MONGO_URI, {
         useNewUrlParser: true,
         useUnifiedTopology: true
